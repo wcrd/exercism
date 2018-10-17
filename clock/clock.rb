cbe75860998b9a922a400b75aaaa5afa07327d1b
William Davidson <william.cr.davidson@gmail.com>
2018-10-17 21:51:50 +1100

    Update init, add hour and minute methods

--- clock/clock.rb
+++ clock/clock.rb
@@ -1,14 +1,13 @@
 # Clock class stores the time in hh:mm 12h format
 class Clock
-  attr_reader :hour, :minute
+  attr_reader :total_minutes
 
   def initialize(hour: 0, minute: 0)
-    time_in_minutes = parse_time(hour, minute)
-    @hour, @minute = time_in_minutes.divmod(60)
+    @total_minutes = parse_time(hour, minute)
   end
 
   def to_s
-    "#{format('%02d', @hour)}:#{format('%02d', @minute)}"
+    format('%02d:%02d', hour, minute)
   end
 
   def ==(other)
@@ -16,11 +15,11 @@ class Clock
   end
 
   def +(other)
-    Clock.new(hour: @hour + other.hour, minute: @minute + other.minute)
+    Clock.new(minute: @total_minutes + other.total_minutes)
   end
 
   def -(other)
-    Clock.new(hour: @hour - other.hour, minute: @minute - other.minute)
+    Clock.new(minute: @total_minutes - other.total_minutes)
   end
 
   private
@@ -29,5 +28,13 @@ class Clock
     time_in_minutes = hours * 60 + minutes
     # normalise to 24hrs (1440 minutes)
     time_in_minutes % 1440
+  end
+
+  def hour
+    @total_minutes / 60
+  end
+
+  def minute
+    @total_minutes % 60
   end
 end
